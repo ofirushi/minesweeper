a84c22abeb2229f3428b15cfbf984313a8fd68dc
ofirushi <71781709+ofirushi@users.noreply.github.com>
2020-09-23 19:31:10 +0300

    1st-delivery2.0

--- js/main.js
+++ js/main.js
@@ -54,12 +54,12 @@ function buildBoard() {
 }
 
 function checkVictory() {
-    console.log('Checking Victory',gLevel.SIZE,gGame.shownCount,gGame.markedCount)
+
 
     if (gLevel.SIZE ** 2 - gGame.shownCount === gGame.markedCount) {
         document.querySelector('.board').style.pointerEvents = "none"
     stopStopper()
-console.log('you won')
+alert('You won!')
     }
 }
 
@@ -111,12 +111,10 @@ function isNeg(cell1I, cell1J, cell2I, cell2J, mat) {
             if (j < 0 || j >= mat[i].length) continue;
             if (i === parseInt(cell1I) && j === parseInt(cell1J)) continue;
             if (i === cell2I && j === cell2J) {
-                console.log('neighbor')
                 return true
             }
         }
     }
-    console.log('not neighbor')
     return false;
 }
 
@@ -129,6 +127,7 @@ function mineClicked() {
     }
     document.querySelector('.board').style.pointerEvents = "none"
     stopStopper()
+    alert('You lose!')
 }
 
 function firstMove(cellId) {
@@ -218,125 +217,4 @@ function getRandomInt(num1, num2) {
     var min = (num2 > num1) ? num1 : num2
     return (Math.floor(Math.random() * (max - min)) + min)
 }
-
-// var currNumPos = getBoardIdxByNum(cellId, gBoard)
-
-// // console.log(gBoard[prevNumPos[0]][prevNumPos[1]])
-// if (cellId === 1) {
-//     initGame()
-// }
-// if (gLastNum + 1 === cellId) {
-//     gBoard[currNumPos[0]][currNumPos[1]].isMarked = true
-//     elCell.style.background = 'linear-gradient(to bottom, #d0451b 5%, #bc3315 100%)'
-//     elCell.style.backgroundColor = '#000000'
-//     elCell.style.textShadow = 'none'
-//     gLastNum = cellId
-//     if (cellId === gSize ** 2) {
-//         stopStopper()
-//         return alert('You Win!')
-//     }
-
-
-
-
-
-// var gLastNum = 0
-// var ggLevel.SIZE = 4
-// var gIsGameOn = false
-// var gInterval
-// var gTime
-
-
-// function sizeSellector(num) {
-//     gSize = num
-//     gBoard = createGameBoard()
-//     renderBoard(gBoard)
-//     stopStopper()
-//     gLastNum = 0
-//     gIsGameOn = false
-// }
-
-
-
-// function cellClicked(elCell) {
-//     var cellId = parseInt(elCell.id)
-//     var currNumPos = getBoardIdxByNum(cellId, gBoard)
-
-//     // console.log(gBoard[prevNumPos[0]][prevNumPos[1]])
-//     if (cellId === 1) {
-//         initGame()
-//     }
-//     if (gLastNum + 1 === cellId) {
-//         gBoard[currNumPos[0]][currNumPos[1]].isMarked = true
-//         elCell.style.background = 'linear-gradient(to bottom, #d0451b 5%, #bc3315 100%)'
-//         elCell.style.backgroundColor = '#000000'
-//         elCell.style.textShadow = 'none'
-//         gLastNum = cellId
-//         if (cellId === gSize ** 2) {
-//             stopStopper()
-//             return alert('You Win!')
-//         }
-//     }
-
-
-// }
-
-// function initGame() {
-
-//     if (gIsGameOn) return
-//     startStopper()
-//     gIsGameOn = true
-// }
-
-// function startStopper() {
-
-//     var startTime = Date.now()
-
-//     gInterval = setInterval(function () {
-//         var elapsedTime = Date.now() - startTime
-//         document.getElementById("timer").innerHTML = (elapsedTime / 1000).toFixed(3)
-//     }, 1)
-
-// }
-
-// function stopStopper() {
-//     clearInterval(gInterval)
-
-//     return gInterval
-// }
-
-// function renderBoard(board) {
-//     // console.table(board)
-//     var htmlStr = ''
-//     for (var i = 0; i < board.length; i++) {
-//         htmlStr += '<tr>'
-//         var row = board[i]
-//         for (var j = 0; j < board.length; j++) {
-//             var cell = row[j].num
-//             htmlStr += '<td onclick="cellClicked(this)" id="' + cell + '">' + cell + '</td>'
-//         }
-//         htmlStr += '</tr>'
-//     }
-
-
-//     var elBoard = document.querySelector('.board')
-//     elBoard.innerHTML = htmlStr
-// }
-
-// 
-
-// function getBoardIdxByNum(x, board) {
-//     for (var i = 0; i < board.length; i++) {
-//         var row = board[i]
-//         for (var j = 0; j < row.length; j++) {
-//             // console.log(row[j], 'asd', row[j].num)
-//             if (x === row[j].num) {
-//                 var pos = [i, j]
-//                 return pos
-//             }
-
-//         }
-//     }
-// }
-
 
